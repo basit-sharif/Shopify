86366e9a4326cf280d048a81b7d645ef0079bd5b
Abdul-Basit <ba3568008@gmail.com>
2023-03-07 02:34:40 +0500

    updated buttons

--- navbar/src/views/navbarView/index.tsx
+++ navbar/src/views/navbarView/index.tsx
@@ -59,22 +59,22 @@ export default function NavbarView({ navItem }: typeofNavItems) {
 function OffCanvasSidebarMobile({ data, sidebar, setSidebar }: any) {
   return (
     <>
-      <div onMouseLeave={() => { setSidebar(false) }} className={`${sidebar ? "flex" : "hidden"} flex-col items-end fixed top-0 right-0 px-6 w-80 h-screen overflow-y-auto bg-white`}>
-        <div onClick={() => { setSidebar(!sidebar) }} className="flex justify-end w-full border-b-2 cursor-pointer py-5 transition duration-75 " >
-          <div className="p-4 active:bg-gray-200">
+      <div onMouseLeave={() => { setSidebar(false) }} className={`${sidebar ? "flex" : "hidden"} flex-col items-end fixed top-0 right-0 px-2 w-80 h-screen overflow-y-auto bg-white`}>
+        <div className="flex justify-end w-full border-b-2 cursor-pointer py-5 transition duration-75 " >
+          <div onClick={() => { setSidebar(!sidebar) }} className="p-4 active:bg-gray-200">
             <GrClose />
           </div>
         </div>
-        <ul className="space-y-2 w-full">
+        <ul className=" w-full">
           {data.map((item: any) =>
-            <li className="cursor-pointer flex justify-between w-full border-b-2 py-4 transition duration-75 active:bg-gray-200 " key={item.label}>
+            <li className="rounded-sm cursor-pointer flex justify-between w-full border-b-2 p-4 transition duration-75 active:bg-gray-200 " key={item.label}>
               <h4>{item.label}</h4>
               {item.dropdown ? <RiArrowDropDownLine size={30} /> : ""}
 
             </li>
           )}
         </ul>
-        <div className="cursor-pointer flex justify-start w-full py-6 transition duration-75 active:bg-gray-200 space-x-5 " >
+        <div className="cursor-pointer flex justify-start w-full p-4 space-x-5 " >
           <BsFacebook size={25} />
           <FaTwitter size={25} />
         </div>
