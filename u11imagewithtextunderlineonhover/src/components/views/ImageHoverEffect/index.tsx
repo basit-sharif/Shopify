61c2cd45f06900c242a7fcba68538fc9dbba1e36
Abdul-Basit <ba3568008@gmail.com>
2023-03-15 02:09:11 +0500

    added deployed linkl

--- u11imagewithtextunderlineonhover/src/components/views/ImageHoverEffect/index.tsx
+++ u11imagewithtextunderlineonhover/src/components/views/ImageHoverEffect/index.tsx
@@ -7,6 +7,7 @@ export default function ImageHoverEffect() {
                 <div className="cursor-pointer col-span-2 sm:col-start-1 sm:col-end-3">
                     <div className="relative group">
                         <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/mens-Stocksy_txpd2ea9debXoZ100_Medium_222225-edited_1512x.jpg?v=1500224081" className="max-h-80 object-cover" width={1000} height={1000} alt="image" />
+                        <div className="dullness"></div>
                         <div className="absolute bottom-7 left-6">
                             <h2>
                                 Men
@@ -16,6 +17,7 @@ export default function ImageHoverEffect() {
                 </div>
                 <div className="cursor-pointer relative group w-full">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/pullovers_720x.jpg?v=1500223091" className="image-prop" width={1000} height={1000} alt="image" />
+                    <div className="dullness"></div>
                     <div className="absolute bottom-7 left-6">
                         <h2>
                             Pullovers
@@ -32,6 +34,7 @@ export default function ImageHoverEffect() {
                 </div>
                 <div className="cursor-pointer relative group w-full bg-pink-500">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/camp_720x.jpg?v=1500223599" className="image-prop" width={1000} height={1000} alt="image" />
+                    <div className="dullness"></div>
                     <div className="absolute bottom-7 left-6">
                         <h2>
                             Camp
@@ -40,6 +43,7 @@ export default function ImageHoverEffect() {
                 </div>
                 <div className="cursor-pointer relative group w-full bg-yellow-800">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/backpacks_720x.jpg?v=1500223563" className="image-prop" width={1000} height={1000} alt="image" />
+                    <div className="dullness"></div>
                     <div className="absolute bottom-7 left-6">
                         <h2>
                             Backpacks
@@ -48,6 +52,7 @@ export default function ImageHoverEffect() {
                 </div>
                 <div className="cursor-pointer relative group col-span-2 sm:col-start-3 sm:col-end-5 bg-black">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/womens-edited_1512x.jpg?v=1500224178" className="max-h-80 object-cover" width={1000} height={1000} alt="image" />
+                    <div className="dullness"></div>
                     <div className="absolute bottom-7 left-6">
                         <h2>
                             Women
