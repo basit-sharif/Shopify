af91c846ce971f29e7d330d7797cd900a6965ef8
Abdul-Basit <ba3568008@gmail.com>
2023-03-17 01:22:35 +0500

    changed to dynamicly

--- u11imagewithtextunderlineonhover/src/components/views/ImageHoverEffect/index.tsx
+++ u11imagewithtextunderlineonhover/src/components/views/ImageHoverEffect/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import { useState } from "react";
+import { ImageData, ImageDataType } from "@/components/typesAndArrays/ImageData";
 
 export default function ImageHoverEffect() {
     let [scalingAnimation, setScalingAnimation] = useState("scale-125");
@@ -21,8 +22,10 @@ export default function ImageHoverEffect() {
     return (
         <div className="w-full p-3 sm:p-12">
             <div className="mx-auto max-w-7xl grid grid-cols-2 sm:grid-cols-4 overflow-hidden">
-                <div className="cursor-pointer col-span-2 sm:col-start-1 sm:col-end-3">
-                    <div className="overflow-hidden relative group">
+
+                {/* This is static */}
+
+                {/* <div className="cursor-pointer relative group w-full overflow-hidden col-span-2 sm:col-start-1 sm:col-end-3">
                         <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/mens-Stocksy_txpd2ea9debXoZ100_Medium_222225-edited_1512x.jpg?v=1500224081" className={`max-h-80 object-cover ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
                         <div className="dullness"></div>
                         <div className="absolute bottom-4 sm:bottom-7 left-4 sm:left-6">
@@ -30,7 +33,6 @@ export default function ImageHoverEffect() {
                                 Men
                             </h2>
                         </div>
-                    </div>
                 </div>
                 <div className="cursor-pointer relative group w-full overflow-hidden">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/pullovers_720x.jpg?v=1500223091" className={`image-prop ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
@@ -49,7 +51,7 @@ export default function ImageHoverEffect() {
                         </h2>
                     </div>
                 </div>
-                <div className="overflow-hidden cursor-pointer relative group w-full bg-pink-500">
+                <div className="overflow-hidden cursor-pointer relative group w-full">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/camp_720x.jpg?v=1500223599" className={`image-prop ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
                     <div className="dullness"></div>
                     <div className="absolute bottom-4 sm:bottom-7 left-4 sm:left-6">
@@ -58,7 +60,7 @@ export default function ImageHoverEffect() {
                         </h2>
                     </div>
                 </div>
-                <div className="overflow-hidden cursor-pointer relative group w-full bg-yellow-800">
+                <div className="overflow-hidden cursor-pointer relative group w-full">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/backpacks_720x.jpg?v=1500223563" className={`image-prop ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
                     <div className="dullness"></div>
                     <div className="absolute bottom-4 sm:bottom-7 left-4 sm:left-6">
@@ -67,7 +69,7 @@ export default function ImageHoverEffect() {
                         </h2>
                     </div>
                 </div>
-                <div className="overflow-hidden cursor-pointer relative group col-span-2 sm:col-start-3 sm:col-end-5 bg-black">
+                <div className="overflow-hidden cursor-pointer relative group w-full col-span-2 sm:col-start-3 sm:col-end-5 bg-black">
                     <Image src="https://cdn.shopify.com/s/files/1/2091/0251/collections/womens-edited_1512x.jpg?v=1500224178" className={`max-h-80 object-cover ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
                     <div className="dullness"></div>
                     <div className="absolute bottom-4 sm:bottom-7 left-4 sm:left-6">
@@ -75,7 +77,22 @@ export default function ImageHoverEffect() {
                             Women
                         </h2>
                     </div>
-                </div>
+                </div> */}
+
+
+                {/* This is dynamic */}
+
+                {ImageData.map((item: ImageDataType, index: number) =>
+                    <div key={index + 2} className={`${item.colSpan} ${item.TabColStart} ${item.TabColSpanEnd} overflow-hidden cursor-pointer relative group w-full`}>
+                        <Image src={item.imageUrl} className={`${item.maxHeight ? "max-h-80 object-cover" : "image-prop"} ${scalingAnimation} duration-1000`} width={1000} height={1000} alt="image" />
+                        <div className="dullness"></div>
+                        <div className="absolute bottom-4 sm:bottom-7 left-4 sm:left-6">
+                            <h2>
+                                {item.label}
+                            </h2>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
